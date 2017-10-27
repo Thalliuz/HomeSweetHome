c5691464e7b8b592dafa665f5555e486e93c8d89
Michael Angelo <michael.angelo@nytimes.com>
2017-10-27 10:13:28 -0400

    fix login

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
   layout "application"
-  
+  skip_before_action :verify_authenticity_token  
+
   def index
   end
   
   def login
-    @user = User.find_by(username: params[:username]) || @owner = Owner.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username])
+    @owner = Owner.find_by(username: params[:username])
     session[:id] = @user.id || @owner.id
     if @owner 
       redirect_to "/owners/#{@owner.id}"
