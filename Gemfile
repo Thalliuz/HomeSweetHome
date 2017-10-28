7920d0612fada2d791fb4d9939e5693ab05ec9d7
SparklingShadow <mathu.d@gmail.com>
2017-10-28 18:57:12 -0400

    Add twilio api

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'bcrypt'
+gem 'twilio-ruby'
+gem 'dotenv-rails'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,13 +1,14 @@
 class IssuesController < ActionController::Base
   layout "application"
+  require 'twilio-ruby'
   
   def index
     @issues = Issue.all
   end
 
   def show
-    id = session[:user_id]
-    @issues = User.find(id).issues
+    id = params[:id]
+    @issue = Issue.find(id)  
   end
   
   def new
@@ -20,6 +21,7 @@ class IssuesController < ActionController::Base
     @user = User.find(session[:id])
     @issue = Issue.new(issue_params)
     if @issue.save
+      twilio_call
       redirect_to "/users/#{@user.id}"
     else
       render "new"
@@ -27,7 +29,21 @@ class IssuesController < ActionController::Base
   end
 end
 
+
 private
+
+def twilio_call
+  account_sid = ENV['ACCOUNT_SID']
+  auth_token = ENV['AUTH_TOKEN']
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+  @client.api.account.messages.create(
+    from: '+18566662318',
+    to: '+18564724210',
+    body: 'Hey there!'
+  )
+end 
 
 def issue_params
   params.require(:issue).permit(:name, :address, :detail, :user_id, :owner_id)
