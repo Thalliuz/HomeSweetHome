734b371de851ebddf6a9a8c1993e449213899058
SparklingShadow <mathu.d@gmail.com>
2017-10-28 17:59:57 -0400

    Add pre-filled Users full name and address to issues, Add Full_name method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :issues
   has_one :owner, through: :property
   has_secure_password
+
+  def full_name
+    self.firstname + " " + self.lastname
+  end
 end
